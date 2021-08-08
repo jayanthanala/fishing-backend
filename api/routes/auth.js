932895d74bd69e3d5f484ab572359d8f4d5cb87e
Jayanth Anala <javaoneapl@gmail.com>
2021-08-08 20:16:09 +0530

    Update auth.js

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -7,16 +7,4 @@ router.post('/register', controllers.register)
 
 router.post('/login', controllers.login)
 
-// router.post('/verifyEmail', controllers.sendEmail)
-
-// router.get('/verify/:code', controllers.verifyEmail)
-
-// // sends the link to the user for changing their password
-// router.post('/resetPassword', controllers.sendResetPasswordLink)
-
-// // sends the password page
-// router.get('/changePassword/:otp', controllers.changePasswordPage)
-
-// router.post('/changePassword', controllers.changePassword)
-
 module.exports = router
